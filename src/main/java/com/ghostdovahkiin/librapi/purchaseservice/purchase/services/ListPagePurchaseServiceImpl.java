355744d2f5bbe13287772e1c3500a12aaeecdb76
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-03-04 17:28:55 -0300

    :recycle: refactor: ListPagePurchaseService using findAll + Exception

---
+++ src/main/java/com/ghostdovahkiin/librapi/purchaseservice/purchase/services/ListPagePurchaseServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ghostdovahkiin.librapi.purchaseservice.purchase.services;
+
+import com.ghostdovahkiin.librapi.purchaseservice.feign.GetBook;
+import com.ghostdovahkiin.librapi.purchaseservice.feign.GetUser;
+import com.ghostdovahkiin.librapi.purchaseservice.purchase.PurchaseRepository;
+import com.ghostdovahkiin.librapi.purchaseservice.purchase.PurchaseReturnDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ListPagePurchaseServiceImpl implements ListPagePurchaseService{
+    private final PurchaseRepository purchaseRepository;
+    private final GetBook getBook;
+    private final GetUser getUser;
+
+    @Override
+    public Page<PurchaseReturnDTO> findPurchase(Pageable pageable) {
+        List<PurchaseReturnDTO> purchaseReturnDTOList =  purchaseRepository.findAllPurchase(getUser, getBook);
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), purchaseReturnDTOList.size());
+        return new PageImpl(purchaseReturnDTOList.subList(start, end), pageable, purchaseReturnDTOList.size());
+    }
+}

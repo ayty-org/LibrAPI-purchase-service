988e71cdb9dd399916aaec93e321be259935c555
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-03-04 18:38:18 -0300

    :fire: feat: Controller DONE

--- src/main/java/com/ghostdovahkiin/librapi/purchaseservice/purchase/v1/PurchaseController.java
+++ src/main/java/com/ghostdovahkiin/librapi/purchaseservice/purchase/v1/PurchaseController.java
@@ -2,17 +2,22 @@ package com.ghostdovahkiin.librapi.purchaseservice.purchase.v1;
 
 import com.ghostdovahkiin.librapi.purchaseservice.purchase.PurchaseReturnDTO;
 import com.ghostdovahkiin.librapi.purchaseservice.purchase.PurchaseSaveDTO;
+import com.ghostdovahkiin.librapi.purchaseservice.purchase.PurchaseUpdateDTO;
+import com.ghostdovahkiin.librapi.purchaseservice.purchase.services.DeletePurchaseService;
 import com.ghostdovahkiin.librapi.purchaseservice.purchase.services.GetPurchaseService;
 import com.ghostdovahkiin.librapi.purchaseservice.purchase.services.ListPagePurchaseService;
 import com.ghostdovahkiin.librapi.purchaseservice.purchase.services.ListPurchaseService;
 import com.ghostdovahkiin.librapi.purchaseservice.purchase.services.SavePurchaseService;
+import com.ghostdovahkiin.librapi.purchaseservice.purchase.services.UpdatePurchaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -29,6 +34,8 @@ public class PurchaseController {
     private final ListPurchaseService listPurchaseService;
     private final ListPagePurchaseService listPagePurchaseService;
     private final GetPurchaseService getPurchaseService;
+    private final DeletePurchaseService deletePurchaseService;
+    private final UpdatePurchaseService updatePurchaseService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -49,4 +56,14 @@ public class PurchaseController {
     @GetMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public PurchaseReturnDTO findOne(@PathVariable("id") Long id) { return getPurchaseService.findByID(id);}
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Long id) { deletePurchaseService.delete(id);}
+
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/{id}")
+    public void update(@Valid @RequestBody PurchaseUpdateDTO purchaseUpdateDTO, @PathVariable Long id) {
+        updatePurchaseService.update(purchaseUpdateDTO, id);
+    }
 }
